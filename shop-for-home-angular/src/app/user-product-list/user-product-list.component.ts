33810ca347565905cb59ebc2f2c3587de799bdf9
bdappelhans <bdappelhans@gmail.com>
2024-08-21 22:15:41 -0500

    wish list functionality added to user product list page

--- shop-for-home-angular/src/app/user-product-list/user-product-list.component.ts
+++ shop-for-home-angular/src/app/user-product-list/user-product-list.component.ts
@@ -10,17 +10,20 @@ import { AuthService } from '../service/auth.service';
 import { OrderService } from '../service/order.service';
 import { FormsModule } from '@angular/forms';
 import { OrderProduct } from '../model/order-product';
+import { WishList } from '../model/wish-list';
+import { WishListService } from '../service/wish-list.service';
 
 interface OrderProductDetails {
     orderProduct: OrderProduct,
-    alreadyInCart: boolean
+    alreadyInCart: boolean,
+    alreadyInWishList: boolean
 }
 
 @Component({
   selector: 'app-admin-product-list',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HttpClientModule, RouterModule, FormsModule],
-  providers: [ProductService, AuthService, OrderService],
+  providers: [ProductService, AuthService, OrderService, WishListService],
   templateUrl: './user-product-list.component.html',
   styleUrls: ['./user-product-list.component.css']
 })
@@ -29,8 +32,10 @@ export class UserProductListComponent implements OnInit {
   user: User | null = null;
   currentOrder: Order | null = null;
   opDetails: { [productId: number]: OrderProductDetails } = {};
+  wishList: WishList[] = [];
 
-  constructor(private productService: ProductService, private authService: AuthService, private orderService: OrderService, private router: Router) {}
+  constructor(private productService: ProductService, private authService: AuthService, private orderService: OrderService, 
+    private router: Router, private wishService: WishListService) {}
 
   ngOnInit(): void {
     this.fetchUser();
@@ -52,15 +57,25 @@ export class UserProductListComponent implements OnInit {
   createProductDetailsMap(): void {
     for (const product of this.products) {
       let orderProduct = this.currentOrder?.orderProducts.find(op => op.id.productId === product.id);
+      let alreadyInWishList = false;
+
+      // check to see if item is already in wish list
+      let wishIndex = this.wishList.findIndex(wishItem => wishItem.id.productId == product.id);
+
+      // if product found in wish list, set alreadyInWishList property to true
+      if (wishIndex != -1) {
+        alreadyInWishList = true;
+      }
+
       let opDetails: OrderProductDetails;
 
       if (orderProduct) {
-        opDetails = { orderProduct, alreadyInCart: true };
+        opDetails = { orderProduct, alreadyInCart: true, alreadyInWishList };
         this.opDetails[product.id] = opDetails;
       } else {
         if (this.currentOrder) {
           let orderProduct = {id: { orderId: this.currentOrder?.id, productId: product.id}, quantity: 1 };
-          opDetails = { orderProduct: orderProduct, alreadyInCart: false };
+          opDetails = { orderProduct: orderProduct, alreadyInCart: false, alreadyInWishList };
           this.opDetails[product.id] = opDetails;
         }
       }
@@ -89,10 +104,25 @@ export class UserProductListComponent implements OnInit {
         (order: Order) => {
           console.log("Unplaced order: ", order);
           this.currentOrder = order;
-          this.fetchProducts();
+          this.fetchWishList();
         },
         (error) => {
           console.error('Error fetching order:', error);
+        }
+      );
+    }
+  }
+
+  fetchWishList(): void {
+    if (this.user) {
+      this.wishService.getWishListByUserId(this.user.id).subscribe(
+        (wishList: WishList[]) => {
+          console.log('Wish List:', wishList);
+          this.wishList = wishList;
+          this.fetchProducts();
+        },
+        (error) => {
+          console.error('Error fetching wish list:', error);
         }
       );
     }
@@ -125,6 +155,71 @@ export class UserProductListComponent implements OnInit {
       );
     }
   }
+
+  removeFromCart(productId: number): void {
+    if (this.currentOrder) {
+      // Filter out the product from the order's products list
+      this.currentOrder.orderProducts = this.currentOrder.orderProducts.filter(op => op.id.productId !== productId);
+  
+      // Update the order via the order service
+      this.orderService.updateOrder(this.currentOrder).subscribe(
+        () => {
+          // Update the opDetails map if the currentOrder exists
+          if (this.currentOrder) {
+            this.opDetails[productId] = {
+              orderProduct: {
+                id: { orderId: this.currentOrder.id, productId: productId },
+                quantity: 1
+              },
+              alreadyInCart: false,
+              alreadyInWishList: this.opDetails[productId].alreadyInWishList
+            };
+          }
+  
+          // Alert the user that the cart was successfully updated
+          alert("Cart successfully updated!");
+        },
+        (error) =>  { 
+          console.error('Error updating order:', error);
+        }
+      );
+    }
+  }
+
+  handleWishList(productId: number): void {
+
+    if (this.user) {
+      const wishItem: WishList = {id: {productId: productId, userId: this.user.id}}
+
+      if (this.opDetails[productId].alreadyInWishList) {
+
+        this.wishService.deleteWishItem(wishItem).subscribe({
+          next: () => {
+            this.opDetails[productId].alreadyInWishList = false;
+            this.wishList.filter(wishItem => wishItem.id.productId === productId);
+          },
+          error: err => {
+            console.error('Failed to remove wish item:', err);
+          }
+        });
+  
+      } else {
+      
+        this.wishService.addWishItem(wishItem).subscribe(
+          (response) => {
+            console.log('Wish item added successfully:', response);
+            this.opDetails[productId].alreadyInWishList = true;
+            this.wishList.push(wishItem);
+          },
+          (error) => {
+            console.error('Error saving wish item:', error);
+          }
+        );
+      }
+    }
+
+  }
+  
 
   viewProduct(productId: number): void {
     this.router.navigate(['/user/shop/product', productId])
